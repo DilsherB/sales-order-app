75f937d6c9803b8647f90e330e1fddfdf0179914
Dilsher Balouch <dilsherb@gmail.com>
2023-11-17 08:38:17 +0300

    some customers added.

--- src/data/customers.js
+++ src/data/customers.js
@@ -1,6 +1,9 @@
 const customers = [
-  {code: "1011070001", name: "LULU HYPERMARKET (L.L.C) - ARAMCO"},
-  {code: "1011073710", name: "LULU Saudi Hypermarket - Logistics(W/H)"},
+  { code: "1011070001", name: "LULU HYPERMARKET (L.L.C) - ARAMCO" },
+  { code: "1011073710", name: "LULU Saudi Hypermarket - Logistics(W/H)" },
+  { code: "1011073780", name: "LULU Saudi Hypermarket - Damman" },
+  { code: "1011073760", name: "LULU Saudi Hypermarket - Riyadh" },
+  { code: "1011073790", name: "LULU Saudi Hypermarket - Khobar" },
 ];
 
 export default customers;
